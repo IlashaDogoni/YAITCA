ef6a8c3041bbfbfc2301c93c848c52f7022ed9ba
IlashaDogoni <fistonfire@mail.ru>
2021-12-20 22:57:59 +0600

    Timer

--- YAITCA/ViewController.swift
+++ YAITCA/ViewController.swift
@@ -3,6 +3,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    var timer = 10
+    let eggTimes = ["Soft": 300, "Medium": 480, "Hard": 720]
+    var secondsRemaining = 60
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        secondsRemaining = eggTimes[hardness!]!
+       
+         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
+    
+    @objc func update() {
+        if(secondsRemaining > 0) {
+            print("\(secondsRemaining) seconds is remaining")
+            secondsRemaining -= 1
+        }
+    }
 }
